d8226418011e0fbd37486444292553e3b1cc7551
JackTGG <zackgozelski@icloud.com>
2021-11-04 15:47:01 -0700

    update stuff

--- src/main/java/com/Jack/FilledToTheBrim/mixin/ShulkerBoxSlotMixin.java
+++ src/main/java/com/Jack/FilledToTheBrim/mixin/ShulkerBoxSlotMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 
-@Mixin({ShulkerBoxSlot.class})
+@Mixin(ShulkerBoxSlot.class)
 public class ShulkerBoxSlotMixin {
 
 
