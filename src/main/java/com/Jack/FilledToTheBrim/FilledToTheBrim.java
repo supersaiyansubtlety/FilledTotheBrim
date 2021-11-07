f63fcef483edab55f9da7a786a170e6215730b67
JackTGG <zackgozelski@icloud.com>
2021-11-06 17:33:11 -0700

    move stuff around and Bump Version

--- src/main/java/com/Jack/FilledToTheBrim/FilledToTheBrim.java
+++ src/main/java/com/Jack/FilledToTheBrim/FilledToTheBrim.java
@@ -2,14 +2,20 @@ package com.Jack.FilledToTheBrim;
 
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FilledToTheBrim implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("FilledToTheBrim");
+
+    public static boolean IS_ENABLED = true;
 
 
     @Override
     public void onInitialize() {
         //not Logger I know...
-        System.out.println("Filled To The Brim Loaded!");
+        LOGGER.info("Filled To The Brim Loaded!");
 
     }
 }
