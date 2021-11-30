ce6c92780e866b1e97b77206bf91baaacdc2bc1d
JackTGG <zackgozelski@icloud.com>
2021-11-29 20:52:02 -0800

    Bump, "Fix" Incompatibility with other mods.

--- src/main/java/com/Jack/FilledToTheBrim/mixin/ScreenHandlerMixin.java
+++ src/main/java/com/Jack/FilledToTheBrim/mixin/ScreenHandlerMixin.java
@@ -3,8 +3,6 @@ package com.Jack.FilledToTheBrim.mixin;
 
 import com.Jack.FilledToTheBrim.FilledToTheBrim;
 import com.Jack.FilledToTheBrim.util.nbtutil;
-import net.minecraft.block.ShulkerBoxBlock;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,14 +16,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class ScreenHandlerMixin {
     @Redirect(method = "calculateComparatorOutput(Lnet/minecraft/inventory/Inventory;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getMaxCount()I"))
     private static int GetMaxCount(ItemStack itemStack) {
-        if (FilledToTheBrim.SHULKERBOX_COMPARATOR_OVERLOAD_FIX && nbtutil.isEmptyShulkerBox(itemStack)) {
-            return FilledToTheBrim.SHULKERBOX_STACK_SIZE;
+        if (!FilledToTheBrim.SHULKERBOX_COMPARATOR_OVERLOAD_FIX && nbtutil.isEmptyShulkerBox(itemStack)) {
+            return 1;
         }
         return itemStack.getMaxCount();
 
-        //Code From Essential Addons, Still want to include in Filled To The Brim
-        //if (!FilledToTheBrim.SHULKERBOX_COMPARATOR_OVERLOAD_FIX && itemStack.getItem() instanceof BlockItem && ((BlockItem) itemStack.getItem()).getBlock() instanceof ShulkerBoxBlock)
-        //    return 1;
-        //return itemStack.getMaxCount();
     }
 }
